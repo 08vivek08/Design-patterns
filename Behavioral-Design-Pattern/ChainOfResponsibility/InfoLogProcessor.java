f496bffe52c818c7abc2885bf6abbc975ff34191
Vivek <08vivek08@gmail.com>
2024-07-27 23:55:41 +0530

    ChainOfResponsibility design pattern

---
+++ Behavioral-Design-Pattern/ChainOfResponsibility/InfoLogProcessor.java
@@ -0,0 +1,21 @@
+package ChainOfResponsibility;
+
+public class InfoLogProcessor extends LogProcessor
+{
+    public InfoLogProcessor(LogProcessor nextLogProcessor)
+    {
+        super(nextLogProcessor);
+    }
+
+    public void log(int logLevel, String message)
+    {
+        if(logLevel == INFO)
+        {
+            System.out.println("Info: " + message);
+        }
+        else
+        {
+            super.log(logLevel, message);
+        }
+    }
+}
